5fa754360f99db4a958536695657518685337e10
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-05-10 04:54:00 +0200

    Install webpack and add html & css files

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development', // production
+  entry: {
+    main: path.resolve(__dirname, 'src/app.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 8081,
+    open: true,
+    hot: true,
+    watchFiles: ['src/**/*'],
+  },
+  // loaders
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  // plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Exercise',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+  ],
+};
